5b16cf3980678eb9ddf539ef3b0cb942b094e72c
Ravi Sharma <ravi4j@icloud.com>
2017-03-18 19:25:01 -0500

    Two sum - One more approach

--- src/P0001_TwoSum.js
+++ src/P0001_TwoSum.js
@@ -15,14 +15,33 @@ var twoSum = function(nums, target) {
       saved[target - nums[i]] = i
       console.log("saved :" + JSON.stringify(saved));
     }
-  }
+  };
+
+var hasTwoSum = function(nums , target) {
+  	var i = 0 , j = nums.length - 1;
+  	nums.sort(function(a ,b) { return a - b;});
+  	while(i < j){
+  		if(nums[i] + nums[j] === target)
+  			return true;
+  		else if (nums[i] + nums[j] < target)
+  			i++;
+  		else
+  			j--;		
+ 	}
+   	return false;
+  };
+
 
 
 var INPUT = [2, 7, 11, 15] , target = 9;
-var result = twoSum(INPUT ,target);
+//var result = twoSum(INPUT ,target);
+var result = hasTwoSum(INPUT ,target);
 console.log(result);
 
 
-var INPUT = [3, 2, 4] , target = 6;
-var result = twoSum(INPUT ,target);
-console.log(result);
+var INPUT1 = [3, 1, 4] , target = 6;
+//var result1 = twoSum(INPUT1 ,target);
+var result1 = hasTwoSum(INPUT1 ,target);
+console.log(result1);
+
+
