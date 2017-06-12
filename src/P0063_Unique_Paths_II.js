42e4073a82ff987537d6308a8ab9080a1096d98c
ravi4j <ravi4j@icloud.com>
2017-06-12 17:11:33 -0500

    Removed space from 63

--- src/P0063_Unique_Paths_II.js
+++ src/P0063_Unique_Paths_II.js
@@ -6,7 +6,6 @@ var uniquePathsWithObstacles = function(obstacleGrid) {
     
     if(obstacleGrid === null || obstacleGrid.length === 0)
     	return 0;
-
  	var matrix = [];
     var m = obstacleGrid.length , n = obstacleGrid[0].length;
     for(var i = 0 ; i < m ; i++){
@@ -17,7 +16,6 @@ var uniquePathsWithObstacles = function(obstacleGrid) {
     				matrix[i][j] = 1;
     			else
     				matrix[i][j] = 0;
-
     		} else if( i === 0 && j !== 0){
     			if(obstacleGrid[i][j] !== 1)
     				matrix[i][j] = matrix[i][j - 1];
